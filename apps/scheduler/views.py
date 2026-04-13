44b08f56b0c4dca7aba3104c8519bd5a40d40c54
Sadiya <irinjvai@gmail.com>
2026-04-13 10:40:34 +0600

    added a print

--- apps/scheduler/views.py
+++ apps/scheduler/views.py
@@ -146,6 +146,7 @@ def generate_schedule(request):
             garden_data=garden_data,
             schedule_data=generated_payload,
         )
+        print("Garden Data: ", garden_data)  # Debug log
         print("Generated Schedule: ", generated_payload)  # Debug log
         print("Saved Schedule: ", saved_schedule)  # Debug log
         return Response(
